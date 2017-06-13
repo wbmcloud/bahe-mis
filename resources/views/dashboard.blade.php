18f968e867bfd964edb3d833612aee270fe63abf
wangbaoming <wangbaoming@lianjia.com>
2017-06-13 23:24:02 +0800

    fix: 增加dashboard欢迎文字

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,9 @@
 @extends('admin_template')
 
 @section('content')
+    <div class="text-center" style="margin-top: 20%;">
+        <h3>尊敬的代理 {{ Auth::user()->name }} 您好！！！</h3>
+
+        <h4>请在左侧栏选择对应的操作。</h4>
+    </div>
 @endsection
