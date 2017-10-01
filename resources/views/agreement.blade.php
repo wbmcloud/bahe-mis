3bdca71baa455c137c3f2d11c072abcccb8c47bd
wangbaoming <wangbaoming@lianjia.com>
2017-10-01 22:27:15 +0800

    fix: 修改协议title

--- resources/views/agreement.blade.php
+++ resources/views/agreement.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>巴禾游戏 | 代理协议</title>
+    <title>乡乐麻将 | 代理协议</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
     <link rel="stylesheet" href="{{ asset("/bower_components/admin-lte/bootstrap/css/bootstrap.min.css") }}">
