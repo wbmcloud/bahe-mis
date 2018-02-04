1e02c2403d3bc0fa126b72c7e5a7b103d5376d16
wangbaoming <wangbaoming@lianjia.com>
2018-02-04 12:04:36 +0800

    fix: 只有代理显示代理协议

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,5 +5,5 @@
   Anything you want
 </div>
 <!-- Default to the left -->
-<strong>Copyright &copy; 2017 <a href="#">乡乐棋牌</a>.&nbsp;<a style="color: red; text-decoration: underline; font-style: italic" href="/user/agreement">代理协议</a></strong>
+<strong>Copyright &copy; 2017 <a href="#">乡乐棋牌</a>.&nbsp;@role(['agent', 'first_agent', 'general_agent'])<a style="color: red; text-decoration: underline; font-style: italic" href="/user/agreement">代理协议</a>@endrole</strong>
 </footer>
